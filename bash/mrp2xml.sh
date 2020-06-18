74a195b2ded25c24bfdcbf97beee73f3dff34c58
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-06-18 17:06:23 +0200

    Fix paths in script

--- bash/mrp2xml.sh
+++ bash/mrp2xml.sh
@@ -4,7 +4,7 @@ for MRP in $*; do
   mkdir -p ${MRP%.mrp}
   XML=${MRP%.mrp}.xml
   python toolkit/mtool/main.py $MRP $XML --read mrp --write ucca || exit 1
-  csplit -zk $XML '/^<root/' -f '' -b "data/$MRP/%03d.xml" {553}
-  python -m semstr.evaluate $XML $GOLD -qs ${MRP%.mrp}.scores.txt
+  csplit -zk $XML '/^<root/' -f '' -b "${MRP%.mrp}/%03d.xml" {553}
+  python -m semstr.evaluate ${MRP%.mrp} $GOLD -qs ${MRP%.mrp}.scores.txt
 done
 
