cef47a55593cc57101bdbfcdc0c8609dfd24290e
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-06-18 17:09:05 +0200

    Less prints from csplit

--- bash/mrp2xml.sh
+++ bash/mrp2xml.sh
@@ -4,7 +4,7 @@ for MRP in $*; do
   mkdir -p ${MRP%.mrp}
   XML=${MRP%.mrp}.xml
   python toolkit/mtool/main.py $MRP $XML --read mrp --write ucca || exit 1
-  csplit -zk $XML '/^<root/' -f '' -b "${MRP%.mrp}/%03d.xml" {553}
-  python -m semstr.evaluate ${MRP%.mrp} $GOLD -qs ${MRP%.mrp}.scores.txt
+  csplit -zk $XML '/^<root/' -f '' -b "${MRP%.mrp}/%03d.xml" {553} > /dev/null
+  python -m semstr.evaluate -i ${MRP%.mrp} $GOLD -qs ${MRP%.mrp}.scores.txt
 done
 
